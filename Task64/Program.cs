a381de71e937ea738c531746e3442640a5d31631
YuliyaYahshigulova <116660856+YuliyaYahshigulova@users.noreply.github.com>
2022-11-18 14:10:06 +0700

    update Program.cs
    changed the solution

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,22 +1,14 @@
-﻿// Задайте значение N. Напишите программу, 
+// Задайте значение N. Напишите программу, 
 // которая выведет все натуральные числа в промежутке от N до 1. 
 // Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-using static System.Console;
-
-string RecursiveNumbers(int n)
+string Numbers(int n)
 {
-    if (n == 1)
-    {
-        return "1";
-    }
-    else
-    {
-        return n + ", " + RecursiveNumbers(n - 1);
-    }
+    if (n == 1)return "1";
+    else return n + ", " + Numbers (n - 1);
 }
-WriteLine(RecursiveNumbers(23));
+Console.WriteLine(Numbers(5));
 
 
