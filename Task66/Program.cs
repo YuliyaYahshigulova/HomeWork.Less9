7fb8b1894f86236f3a1376864ffc69c5efa6f3f8
YuliyaYahshigulova <116660856+YuliyaYahshigulova@users.noreply.github.com>
2022-11-20 12:59:27 +0700

    update Program.cs
    change the decision of task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,19 +1,11 @@
-﻿// Задайте значения M и N. Напишите программу, 
+// Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-using static System.Console;
-
-int RecursiveSumm(int m, int n)
+int Summ(int m, int n)
 {
-    if (m == n)
-    {
-        return m;
-    }
-    else
-    {
-        return n + RecursiveSumm(m, n - 1);
-    }
+    if (m == n) return m;
+    else return n + Summ(m, n - 1);
 }
-WriteLine(RecursiveSumm(6, 10));
+Console.WriteLine(Summ(4, 8));
