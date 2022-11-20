3d196db82c6fbc6a78ee0f95c4dafdee0cfbce4e
YuliyaYahshigulova <116660856+YuliyaYahshigulova@users.noreply.github.com>
2022-11-20 13:03:49 +0700

    update Program.cs
    change the decision of task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -3,14 +3,9 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-using static System.Console;
-
 int Ackerman(int m, int n)
 {
-    if (m == 0)
-    {
-        return n + 1;
-    }
+    if (m == 0) return n + 1;
     else if (m > 0 & n == 0)
     {
         return Ackerman(m - 1, 1);
@@ -20,7 +15,7 @@ int Ackerman(int m, int n)
         return Ackerman(m - 1, Ackerman(m, n - 1));
     }
 }
-WriteLine(Ackerman(2, 3));
+Console.WriteLine(Ackerman(2, 3));
 
 
 
